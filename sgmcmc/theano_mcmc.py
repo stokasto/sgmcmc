e59b6531e5facb34192b8116ef49fda2a8889cc2
Jost Tobias Springenberg <springj@informatik.uni-freiburg.de>
2016-10-24 16:39:57 +0200

    merge in burn in handling and treatment of mdecay as in the paper

--- sgmcmc/theano_mcmc.py
+++ sgmcmc/theano_mcmc.py
@@ -9,28 +9,36 @@ from .utils import sharedX
 
 class SGHMCSampler(object):
 
-    def __init__(self, rng=None, precondition=False):
+    def __init__(self, rng=None, precondition=False, ignore_burn_in=False):
         if rng:
             self._srng = rng
         else:
             self._srng = RandomStreams(np.random.randint(1, 2147462579))
         self.precondition = precondition
         self.prepared = False
+        self.ignore_burn_in = ignore_burn_in
+        self.steps_burn_in = 0
+        self.requires_burn_in = self.precondition
 
-    def prepare_updates(self, cost, params, epsilon, A=1., inputs=[], scale_grad=1., mdecay=None, **kwargs):
+    def prepare_updates(self, cost, params, epsilon, mdecay=0.05, inputs=[], scale_grad=1.,
+                        A=None, **kwargs):
         self.updates = []
+        self.burn_in_updates = []
         grads = T.grad(cost, params)
         self.params = params
         self.cost = cost
         self.count = sharedX(0)
         self.epsilon = sharedX(np.float32(epsilon))
-        self.A = T.cast(A, theano.config.floatX)
+        self.mdecay = mdecay 
         self.inputs = inputs
-        if mdecay is not None:
-            # calculate A based on mdecay
-            newA = mdecay * np.sqrt(scale_grad) / epsilon 
-            self.A = T.cast(newA, theano.config.floatX)
-            print("You specified mdecay of {} -> changing A to {}".format(mdecay, newA))
+        if A is not None:
+            # calculate mdecay based on A
+            #raise NotImplementedError("TODO")
+            eps_scaled = epsilon / np.sqrt(scale_grad)
+            new_mdecay = A * eps_scaled
+            self.mdecay = T.cast(new_mdecay, theano.config.floatX)
+            print("You specified A of {} -> changing mdecay to {}".format(mdecay, newA))
+            
         for theta,grad in zip(params, grads):
             xi = sharedX(theta.get_value() * 0. + 1)
             g = sharedX(theta.get_value() * 0. + 1)
@@ -42,33 +50,51 @@ class SGHMCSampler(object):
                 g2_t = (1. - r_t) * g2 + r_t * grad**2
                 xi_t = 1 + xi * (1 - g * g / (g2 + 1e-16))
                 Minv = 1. / (T.sqrt(g2 + 1e-16) + 1e-16)
-                self.updates.append((g, g_t))
-                self.updates.append((g2, g2_t))
-                self.updates.append((xi, xi_t))
+                self.burn_in_updates.append((g, g_t))
+                self.burn_in_updates.append((g2, g2_t))
+                self.burn_in_updates.append((xi, xi_t))
                 noise = 0.
             else:
                 Minv = 1.
                 noise = 0.
             self.epsilon_scaled = self.epsilon / T.sqrt(T.cast(scale_grad, dtype=theano.config.floatX))
-            sigma = T.sqrt(2. * self.epsilon_scaled * self.epsilon_scaled * self.epsilon_scaled * (T.square(Minv) * (self.A - noise)))
+            noise_scale = 2. * self.epsilon_scaled ** 2 * self.mdecay * Minv - 2. * self.epsilon_scaled ** 3 * T.square(Minv) * noise
+            sigma = T.sqrt(T.maximum(noise_scale, 1e-16))
             sample_t = self._srng.normal(size=theta.shape) * sigma
-            p_t = p - self.epsilon**2 * Minv *  grad - self.epsilon_scaled * Minv * A * p + sample_t
+            p_t = p - self.epsilon**2 * Minv * grad - self.mdecay * p + sample_t
             theta_t = theta + p_t
             self.updates.append((theta, theta_t))
             self.updates.append((p, p_t))
         self.prepared = True
-        return self.updates
+        if self.ignore_burn_in:
+            return self.updates + self.burn_in_updates
+        else:
+            return self.updates, self.burn_in_updates
 
     def step(self, *inp):
         if not self.prepared:
             raise RuntimeError("You called step() without a prior call to prepare_updates()")
         if not hasattr(self, "step_fun"):
-            #Tinput = [T.matrix() for i in range(len(inp))]
             print("... compiling theano function")
             self.step_fun = theano.function(self.inputs, self.cost, updates=self.updates)
+        if not self.ignore_burn_in and self.steps_burn_in < 1 and self.requires_burn_in:
+            raise RuntimeError("Your sampler requires a burn_in please run step_burn_in() for a few steps")
         nll = self.step_fun(*inp)
         return self.params, nll
 
+    def step_burn_in(self):
+        if not self.prepared:
+            raise RuntimeError("You called step_burn_in() without a prior call to prepare_updates()")
+        if not hasattr(self, "step_fun_burn_in"):
+            print("... compiling theano function")
+            if self.ignore_burn_in:
+                self.step_fun_burn_in = theano.function(self.inputs, self.cost, updates=self.updates)
+            else:
+                self.step_fun_burn_in = theano.function(self.inputs, self.cost, updates=self.updates + self.burn_in_updates)
+        
+        nll = self.step_fun_burn_in(*inp)
+        self.steps_burn_in += 1
+        return self.params, nll
 
 class SGLDSampler(SGHMCSampler):
 
@@ -77,6 +103,7 @@ class SGLDSampler(SGHMCSampler):
 
     def prepare_updates(self, cost, params, epsilon, A=1., inputs=[], scale_grad=1., **kwargs):
         self.updates = []
+        self.burn_in_updates = []
         grads = T.grad(cost, params)
         self.params = params
         self.cost = cost
@@ -94,9 +121,9 @@ class SGLDSampler(SGHMCSampler):
                 g2_t = (1. - r_t) * g2 + r_t * grad**2
                 xi_t = 1 + xi * (1 - g * g / (g2 + 1e-16))
                 Minv = 1. / (T.sqrt(g2 + 1e-16) + 1e-16)
-                self.updates.append((g, g_t))
-                self.updates.append((g2, g2_t))
-                self.updates.append((xi, xi_t))
+                self.burn_in_updates.append((g, g_t))
+                self.burn_in_updates.append((g2, g2_t))
+                self.burn_in_updates.append((xi, xi_t))
                 noise = 0.
             else:
                 Minv = 1.
@@ -106,4 +133,7 @@ class SGLDSampler(SGHMCSampler):
             theta_t = theta - self.epsilon * Minv * self.A * grad + sample_t 
             self.updates.append((theta, theta_t))
         self.prepared = True
-        return self.updates
+        if self.ignore_burn_in:
+            return self.updates + self.burn_in_updates
+        else:
+            return self.updates, self.burn_in_updates
